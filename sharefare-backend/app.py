bd34bf7991a765e3e1d2ce6034c91bab580d1cf3
biyyani <biyyani@biyyanis-MacBook-Air.local>
2025-04-28 20:11:24 +0530

    Initial backend commit

---
+++ sharefare-backend/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sharefare.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from routes import api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
